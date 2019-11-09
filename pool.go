e156b95517c305aa1c6de2a210704edaaa5d12a3
cyprx <cyprx@outlook.com>
2019-11-09 21:41:34 +0700

    mod: fixed size of input and output channel to 1

--- pool.go
+++ pool.go
@@ -15,17 +15,13 @@ type Pool struct {
 }
 
 type PoolConfig struct {
-	Name            string
-	Size            int
-	InputQueueSize  int
-	OutputQueueSize int
+	Name string
+	Size int
 }
 
 func NewPool(config *PoolConfig) *Pool {
 	name := config.Name
 	size := config.Size
-	inputSize := config.InputQueueSize
-	outputSize := config.OutputQueueSize
 	if name == "" {
 		id := uuid.New()
 		name = id.String()
@@ -33,17 +29,11 @@ func NewPool(config *PoolConfig) *Pool {
 	if size == 0 {
 		size = 10
 	}
-	if inputSize == 0 {
-		inputSize = 10
-	}
-	if outputSize == 0 {
-		outputSize = 10
-	}
 	return &Pool{
 		Name:        name,
 		Size:        size,
-		InputQueue:  make(chan Work, inputSize),
-		OutputQueue: make(chan Result, outputSize),
+		InputQueue:  make(chan Work, 1),
+		OutputQueue: make(chan Result, 1),
 		QuitChan:    make(chan bool),
 	}
 }
@@ -56,7 +46,10 @@ func (p *Pool) Start() {
 	<-p.QuitChan
 	dispatcher.Close()
 	log.Println("Pool closed")
+}
 
+func (p *Pool) Submit(w Work) {
+	p.InputQueue <- w
 }
 
 func (p *Pool) Input() chan Work {
